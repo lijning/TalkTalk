8d84a3386e82ab9a9d5860824581d6f912d76e9c
LJN <lijn@zju.edu.cn>
2017-10-29 21:29:21 +0800

    message records.

---
+++ TalkTalk/Model/Record.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace TalkTalk.Model
+{
+    public class Record
+    {
+        List<string> replies, queries;
+        public Record()
+        {
+            replies = new List<string>();
+            queries = new List<string>();
+        }
+        public void AddQuery(string query)
+        {
+            queries.Add(query);
+        }
+        public void AddReply(string reply)
+        {
+            replies.Add(reply);
+        }
+    }
+}
