9a844227c1d1f02e33215fb6bfa6e4646f5e150a
CaptainCandy <33003223+CaptainCandy@users.noreply.github.com>
2017-11-09 16:41:50 +0800

    界面更改2

--- TalkTalk/MessagePage.xaml.cs
+++ TalkTalk/MessagePage.xaml.cs
@@ -26,5 +26,10 @@ namespace TalkTalk
         {
             this.InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            SendText.Text = SendBox.Text;
+        }
     }
 }
